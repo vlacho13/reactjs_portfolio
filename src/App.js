8c62958f8f16fc0714b470acc06161ff49fab258
vlacho13 <striuncekf@hotmail.com>
2018-06-22 00:28:33 -0500

    Included resume page and respective components, excluded about me page

--- src/App.js
+++ src/App.js
@@ -9,18 +9,16 @@ class App extends Component {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title="My Porftolio" scroll>
+        <Header className="header-color" title={<Link style={{textDecoration: "none", color: "white"}} to="/">My PortFolio</Link>}scroll>
             <Navigation>
                 <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
-        <Drawer title="My Drawer">
+        <Drawer title={<Link style={{textDecoration: "none", color: "black"}} to="/">Menu</Link>}> 
             <Navigation>
                 <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
--- src/components/main.js
+++ src/components/main.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router";
 import Landing from "./landingpage";
-import AboutMe from "./aboutme";
 import Contact from "./contact";
 import Projects from "./projects";
 import Resume from "./resume";
@@ -10,7 +9,6 @@ import Resume from "./resume";
 const Main = () => (
     <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path="/aboutme" component={AboutMe} />
         <Route path="/contact" component={Contact} />
         <Route path="/projects" component={Projects} />
         <Route path="/resume" component={Resume} />
